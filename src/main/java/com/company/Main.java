7b058bf00b819523e7b8aeac577ec68214132c67
Trolloldem <g.oldani@studenti.unibg.it>
2020-04-30 19:48:32 +0200

    mergeCodiceVale

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -6,7 +6,7 @@ import org.antlr.v4.runtime.*;
 class Scan{
     public void example() {
         try{
-        String fileIn = "/home/oldem/IdeaProjects/D_and_D_Compiler/src/main/java/com/company/example.txt";
+        String fileIn = "C:\\Users\\gianl\\IdeaProjects\\d_and_d_auto\\src\\main\\java\\com\\company\\example.txt";
         digits4Lexer lexer = new digits4Lexer(CharStreams.fromFileName(fileIn));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         digits4Parser parser = new digits4Parser(tokens);
