3165733d284868c75c1073adde86ba4dea2532e7
Gabriele <paris.gabriele96@gmail.com>
2020-05-05 13:43:33 +0200

    Edit di spelling

--- src/main/java/parsingExceptions/equipMalformedException.java
+++ src/main/java/parsingExceptions/equipMalformedException.java
@@ -2,6 +2,6 @@ package parsingExceptions;
 
 public class equipMalformedException extends RuntimeException {
 	public equipMalformedException(String message, int line) {
-        super("Error: " + message + " > line:" + line);
+        super("line: " + line + " " + message);
     }
 }
